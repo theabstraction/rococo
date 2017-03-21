a55a1b828ef55500fadef60ea5d04638455154e3
Mark Anthony Taylor <mark.anthony.taylor@gmail.com>
2017-03-21 14:30:35 +0000

    Fixed some minor issues in the Release build that stopped compilation

--- dystopia/dystopia.version.cpp
+++ dystopia/dystopia.version.cpp
@@ -1,2 +1,2 @@
-// build.maker generated this file at 14:04:41 on 21/3/2017
-void GetBuildVersion(unsigned& major, unsigned& minor) { major = 0; minor = 1278; }
+// build.maker generated this file at 14:29:45 on 21/3/2017
+void GetBuildVersion(unsigned& major, unsigned& minor) { major = 0; minor = 1284; }
--- dystopia/pre.link.bat
+++ dystopia/pre.link.bat
@@ -2,5 +2,5 @@ cd %1
 .\bin\build.maker.exe %2dystopia.version
 @echo off
 call "C:\VS2015\VC\vcvarsall.bat" amd64
-cl.exe /c /MDd %2dystopia.version.cpp /Fo%1temp/dystopia/version.obj
+cl.exe %3 /c %2dystopia.version.cpp /Fo%1temp/dystopia/version.obj
 
