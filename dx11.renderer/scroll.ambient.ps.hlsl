07c1b85bd6b4e4e95dc621481489aeee13acef0f
Mark Anthony Taylor <mark.anthony.taylor@gmail.com>
2017-11-25 17:06:33 +0000

    Scroll renderer now uses colour alpha to determine ambient blending factor.

--- dx11.renderer/scroll.ambient.ps.hlsl
+++ dx11.renderer/scroll.ambient.ps.hlsl
@@ -63,7 +63,7 @@ float4 per_pixel_lighting(PixelVertex p)
 
 	texel.xyz *= fogging;
 
-	return float4(texel.xyz * ambience.localLight.xyz, texel.w * 0.5f);
+	return float4(texel.xyz * ambience.localLight.xyz, texel.w * p.colour.w);
 }
 
 float4 main(PixelVertex p) : SV_TARGET
