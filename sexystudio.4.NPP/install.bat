73f18245cefd02f4d944338195a95043a034930d
Mark Anthony Taylor <mark.anthony.taylor@gmail.com>
2021-04-28 22:23:37 +0100

    Added Notepad++ template project

---
+++ sexystudio.4.NPP/install.bat
@@ -0,0 +1,3 @@
+cd /D "%~dp0"
+mkdir "C:\Program Files\Notepad++\plugins\sexystudio.4.notepad++"
+copy "..\bin\sexystudio.4.notepad++.dll" "C:\Program Files\Notepad++\plugins\sexystudio.4.notepad++"  
