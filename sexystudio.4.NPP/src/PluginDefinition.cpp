de620582d5714424ef4f740f486c0bff81e02657
Mark Anthony Taylor <mark.anthony.taylor@gmail.com>
2022-03-09 17:50:19 +0000

    SexyStudio, when hosted by Notepad++ will not run intellisense unless the document begins with 'new' or ends with '.sxy'

--- sexystudio.4.NPP/src/PluginDefinition.cpp
+++ sexystudio.4.NPP/src/PluginDefinition.cpp
@@ -382,6 +382,15 @@ void onCharAdded(HWND hScintilla, char c)
 {
     if (sexyIDE)
     {
+        WideFilePath filename;
+        if (SendMessageA(nppData._nppHandle, NPPM_GETFILENAME, WideFilePath::CAPACITY, (LPARAM)filename.buf))
+        {
+            if (!StartsWith(filename, L"new") && !EndsWith(filename, L".sxy"))
+            {
+                return;
+            }
+        }
+
         SendMessageA(hScintilla, SCI_CALLTIPCANCEL, 0, 0);
         SexyEditor_Scintilla editor(hScintilla);
         sexyIDE->UpdateAutoComplete(editor);
