3824e418796e17ad7de376a5aaaef5368144019b
Mark Anthony Taylor <mark.anthony.taylor@gmail.com>
2023-07-22 17:22:37 +0100

    Ogg decoder seems pretty complete, almost. It also handles the case where OGGS are sampled at 11.025kHz and 22.050kHz and scales the buffer to 44100 Hz.

--- source/rococo/rococo.audio/rococo.ogg-vorbis.cpp
+++ source/rococo/rococo.audio/rococo.ogg-vorbis.cpp
@@ -18,11 +18,11 @@ using namespace Rococo::Strings;
 
 namespace AudioAnon
 {
-	struct OggVorbisFile
+	class OggVorbisFile
 	{
 		size_t encodedLength;
 		const uint8* encodedData;
-		OggVorbis_File apiHandle;
+		mutable OggVorbis_File apiHandle;
 		int64 readIndex = 0;
 
 		size_t Read(void* data, size_t sizeofEachElement, size_t numberOfElements)
@@ -106,7 +106,7 @@ namespace AudioAnon
 		{
 			return ((OggVorbisFile*)This)->Tell();
 		}
-		
+	public:
 		OggVorbisFile(cstr utf8Path, const uint8* _encodedData, size_t _encodedLength):
 			encodedData(_encodedData),
 			encodedLength(_encodedLength)
@@ -135,14 +135,52 @@ namespace AudioAnon
 			}
 		}
 
-		~OggVorbisFile()
+		int StreamCount() const
 		{
-			
+			return (int) ov_streams(&apiHandle);
 		}
 
-		void Decode()
+		struct StreamInfo
 		{
+			int channels;
+			int sampleRate;
+		};
 
+		StreamInfo GetStreamInfo(int streamIndex) const
+		{
+			vorbis_info* pInfo = ov_info(&apiHandle, streamIndex);
+			if (pInfo)
+			{
+				return { pInfo->channels, pInfo->rate };
+			}
+			else
+			{
+				return { 0, 0 };
+			}
+		}
+
+		int Decode(uint8* decompressedBuffer, int decompressedCapacity)
+		{
+			enum { USING_SIGNED_SAMPLES = 1 };
+			int bitStream = 0;
+			long result = ov_read(&apiHandle, (char*) decompressedBuffer, decompressedCapacity, !IsEndianLittle(), sizeof int16, USING_SIGNED_SAMPLES, &bitStream);
+			switch (result)
+			{
+			case OV_HOLE:
+				// Corrupt data
+				Throw(0, "Hole: Corrupt file data");
+			case OV_EBADLINK:
+				Throw(0, "Bad link: Corrupt file data");
+			case OV_EINVAL:
+				Throw(0, "Invalid read: Corrupt file data");
+			case 0: /* EOF */
+				break;
+			default:
+				break;
+			}
+
+			int bytesRead = result;
+			return bytesRead;
 		}
 	};
 
@@ -215,7 +253,7 @@ namespace AudioAnon
 		{
 			try
 			{
-				size_t length = DecodeAudioProtected(utf8Path, sample, events, audioBufferManager);
+				size_t length = DecodeAudioProtected(utf8Path, audioBufferManager);
 				if (length == 0)
 				{
 					events.MarkBadSample(sample, "Sample 0 length");					
@@ -230,7 +268,7 @@ namespace AudioAnon
 			}
 		}
 
-		uint32 DecodeAudioProtected(cstr utf8Path, IAudioSample& sample, IAudioSampleEvents& events, IPCMAudioBufferManager& audioBufferManager) 
+		uint32 DecodeAudioProtected(cstr utf8Path, IPCMAudioBufferManager& audioBufferManager) 
 		{
 			// Our algorithm expands the mp3 into a scratch buffer. This grows to accomodate the largest decoded MP3 put through the system
 			// The sample we extract copies the relevant portion of the scratch buffer - this way we ensure the minimum of memory fragmentation.
@@ -241,55 +279,131 @@ namespace AudioAnon
 			}
 			catch (IException& ex)
 			{
-				Throw(ex.ErrorCode(), "%s:\n%s: %s", __FUNCTION__, utf8Path, ex.Message());
+				Throw(ex.ErrorCode(), "%s: %s", __FUNCTION__, ex.Message());
 			}
 
 			size_t length = encodedDataSingleThreadedPrivateHeap.size();
 			if (length == 0)
 			{
-				Throw(0, "%s:\n%s: the file was of zero length", __FUNCTION__, utf8Path);
+				Throw(0, "%s: the file was of zero length", __FUNCTION__);
+			}
+
+			OggVorbisFile f(utf8Path, encodedDataSingleThreadedPrivateHeap.data(), encodedDataSingleThreadedPrivateHeap.size());
+
+			int streamCount = f.StreamCount();
+			if (streamCount != 1)
+			{
+				Throw(0, "Only OGG files with one stream can be decoded. The file had %d.", streamCount);
+			}
+
+			auto info = f.GetStreamInfo(0);
+
+			if (info.sampleRate == 0)
+			{
+				Throw(0, "The OGG file had an unknown sample rate");
+			}
+
+			if (info.channels != (int) nChannels)
+			{
+				Throw(0, "The OGG file had %d channels. The decoder has %u. Bad karma at line infinity.", info.channels, nChannels);
+			}
+
+			size_t scaleFactor = 1;
+
+			switch(info.sampleRate)
+			{
+			case 44100:
+				scaleFactor = 1;
+				break;
+			case 22050:
+				scaleFactor = 2;
+				break;
+			case 11025:
+				scaleFactor = 4;
+				break;
+			default:
+				Throw(0, "The OGG file sample rate was %d Hz. The sample rate needs to be 44100 Hz, 22050 Hz or 11025 Hz for playback.", info.sampleRate);
 			}
 
 			// Assume 11:1 compression ratio. Increase ratio to 12 to try to minimize resizing
 			size_t approxDecompressedSize = (length * 12) & (0xFFFFFF00);
 
-			scratchBuffer.resize(approxDecompressedSize);
-
-			OggVorbisFile f(utf8Path, encodedDataSingleThreadedPrivateHeap.data(), encodedDataSingleThreadedPrivateHeap.size());
-						
-			f.Decode();
-
+			scratchBuffer.resize(approxDecompressedSize * scaleFactor);
+			
 			if (!audioBufferManager.Accept(AudioBufferDescriptor{ nChannels, 16 }))
 			{
-				Throw(0, "%s:\n%s: The audio buffer manager rejected the request to transcode", __FUNCTION__, utf8Path);
+				Throw(0, "%s: The audio buffer manager rejected the request to transcode", __FUNCTION__);
 			}
 			
-			/*
 			uint32 cursor = 0;
-			uint32 currentLen = 0;
 			int32 resizeCount = 0;
+
+			enum { CHUNK_SIZE = 512 };
+			char chunk[CHUNK_SIZE];
+
+			size_t expandedChunkSize = scaleFactor * CHUNK_SIZE;
 
 			while (true)
 			{
-				f.Decode();
-				/*
-				
-				uint32 targetWriteLength = cursor + currentLen;
-				if (targetWriteLength > scratchBuffer.size())
+				if (expandedChunkSize + cursor > scratchBuffer.size())
 				{
-					scratchBuffer.resize(targetWriteLength);
+					scratchBuffer.resize(cursor + expandedChunkSize);
 					resizeCount++;
 				}
 
-				memcpy(scratchBuffer.data() + cursor, pPCMData, currentLen);
-				cursor += currentLen;
+				char* target = scaleFactor != 1 ? chunk : (char*)scratchBuffer.data();
+				int bytesToRead = (int)CHUNK_SIZE;
 
-				pcmBuffer->Unlock();	
+				int nBytesRead = f.Decode((uint8*) target, bytesToRead);
 
-				*/
-	//		}
+				if (nBytesRead == 0)
+				{
+					// EOF
+					break;
+				}
+
+				if ((nBytesRead & 1) != 0)
+				{
+					Throw(0, "Expecting int16s in decoded output, but the number of bytes returned from the ready operation was odd");
+				}
+
+				if (scaleFactor == 1)
+				{
+					cursor += nBytesRead;
+					continue;
+				}
+
+				int16* end = (int16*) (chunk + nBytesRead);
+				int16* reader = (int16*)chunk;
+
+				int16* scratchWriter = (int16*)(scratchBuffer.data() + cursor);
+
+				switch (scaleFactor)
+				{
+				case 2:
+					while (reader < end)
+					{
+						*scratchWriter++ = *reader;
+						*scratchWriter++ = *reader++;
+					}
+					break;
+				case 4:
+					while (reader < end)
+					{
+						*scratchWriter++ = *reader;
+						*scratchWriter++ = *reader;
+						*scratchWriter++ = *reader;
+						*scratchWriter++ = *reader++;
+					}
+					break;
+				default:
+					Throw(0, "Bad scalefactor");
+				}
+
+				cursor += nBytesRead;
+			}
+
 		
-			/*
 			PCMAudioLoadingMetrics metrics{ resizeCount };
 			audioBufferManager.Finalize(metrics);
 
@@ -298,11 +412,7 @@ namespace AudioAnon
 
 			memcpy(target.GetData(), scratchBuffer.data(), cursor);
 			
-			*/
-
-			events.MarkBadSample(sample, "dunno");
-
-			return 0;
+			return cursor;
 		}
 
 		void Free() override
