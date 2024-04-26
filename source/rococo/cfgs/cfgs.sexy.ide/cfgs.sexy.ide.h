588b21b524ac16338373ddfbe424a33ea94fce35
Mark Anthony Taylor <mark.anthony.taylor@gmail.com>
2024-04-26 19:38:17 +0100

    Added comments to the header

--- source/rococo/cfgs/cfgs.sexy.ide/cfgs.sexy.ide.h
+++ source/rococo/cfgs/cfgs.sexy.ide/cfgs.sexy.ide.h
@@ -25,9 +25,15 @@ namespace Rococo::CFGS
 		Colours colours;
 	};
 
+	// Returns true if the anchor is allowed to connect to the target
 	bool IsConnectionPermitted(const CableConnection& anchor, const ICFGSSocket& target, ICFGSDatabase& cfgs, const SexyStudio::ISexyDatabase& db);
-	void ConfigSocket(const ICFGSSocket& socket, SexyStudio::ISexyDatabase& db);
-	void ConfigCFGS(ICFGSDatabase & cfgs, SexyStudio::ISexyDatabase & db);
 
+	// Assign cosmetic choices to a particular socket
+	void ConfigSocket(const ICFGSSocket& socket, SexyStudio::ISexyDatabase& db);
+
+	// Enumerates all functions in the cfgs system and assigns cosmetic choices
+	void ConfigCFGS(ICFGSDatabase &cfgs, SexyStudio::ISexyDatabase & db);
+
+	// For a given typename find the appropriate colours for decorating cables and sockets.
 	Colours GetColoursForType(cstr typeName, SexyStudio::ISexyDatabase& db);
 }
