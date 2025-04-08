ed229ec2d0ce8ce5913421ae6e9fa220b8b386b4
Shyreman <mark.anthony.taylor@gmail.com>
2025-04-08 13:33:39 +0100

    Progress

--- source/rococo/rococo.gui.retained/rococo.gr.carousel.cpp
+++ source/rococo/rococo.gui.retained/rococo.gr.carousel.cpp
@@ -21,6 +21,15 @@ namespace GRANON
 		IGRPanel& panel;
 
 		std::vector<Option> options;
+
+		int optionIndex = 1;
+
+		int optionSpan = 200;
+
+		GRAnchorPadding optionPadding{ 4, 4, 16, 16 };
+
+		IGRWidgetButton* leftButton = nullptr;
+		IGRWidgetButton* rightButton = nullptr;
 		
 		GRCarousel(IGRPanel& owningPanel) : panel(owningPanel)
 		{
@@ -43,6 +52,20 @@ namespace GRANON
 
 		EGREventRouting OnCursorClick(GRCursorEvent& ce) override
 		{
+			if (ce.click.LeftButtonUp)
+			{
+				ForEachRenderedOption([this, &ce](int optionIndex, const GuiRect& edge, const Option& option)
+					{
+						UNUSED(option);
+
+						if (IsPointInRect(ce.position, edge))
+						{
+							this->optionIndex = optionIndex;
+						}
+					}
+				);
+			}
+
 			return EGREventRouting::NextHandler;
 		}
 
@@ -60,7 +83,7 @@ namespace GRANON
 			return EGREventRouting::NextHandler;
 		}
 
-		EGREventRouting OnCursorMove(GRCursorEvent& ce) override
+		EGREventRouting OnCursorMove(GRCursorEvent&) override
 		{
 			return EGREventRouting::NextHandler;
 		}
@@ -75,23 +98,72 @@ namespace GRANON
 			return panel;
 		}
 
+		template<class T>
+		void ForEachRenderedOption(T t)
+		{ 
+			int iOptionsSize = (int)options.size();
+
+			while (optionIndex < 0)
+			{
+				optionIndex += iOptionsSize;
+			}
+
+			while (optionIndex > iOptionsSize)
+			{
+				optionIndex -= iOptionsSize;
+			}
+
+			int pos = -1;
+
+			for (int i = optionIndex - 1; i <= optionIndex + 1; i++, pos++)
+			{
+				if (i >= 0 && i < (int)options.size())
+				{
+					int x = (int32)pos * optionSpan;
+					GuiRect optionRect = panel.AbsRect();
+					optionRect.left = Centre(panel.AbsRect()).x + x - (optionSpan / 2);
+					optionRect.right = optionRect.left + optionSpan;
+
+					GuiRect edge = optionRect;
+					edge.left += optionPadding.left;
+					edge.right -= optionPadding.right;
+					edge.top += optionPadding.top;
+					edge.bottom -= optionPadding.bottom;
+
+					t(i, edge, options[i]);
+				}
+			}
+		}
+
 		void Render(IGRRenderContext& g) override
 		{
-			bool isHovered = g.IsHovered(panel);
-			
-			for (size_t i = 0; i < options.size(); i++)
+			if (options.empty())
 			{
 				GRAlignmentFlags optionTextAlignment;
 				optionTextAlignment.Add(EGRAlignment::HCentre).Add(EGRAlignment::VCentre);
 
-				GRRenderState rs(false, false, false);
-				RGBAb colour = panel.GetColour(EGRSchemeColourSurface::GAME_OPTION_TEXT, rs);
-
-				int x = (int32)i * 200;
-
-				GuiRect optionRect(x, panel.AbsRect().top, x + 200, panel.AbsRect().bottom);
-				g.DrawText(GRFontId::MENU_FONT, optionRect, optionRect, optionTextAlignment, { 0,0 }, to_fstring(options[i].value), colour);
+				RGBAb colour = panel.GetColour(EGRSchemeColourSurface::GAME_OPTION_TEXT, GRRenderState(0, 0, 0));
+				g.DrawText(GRFontId::MENU_FONT, panel.AbsRect(), panel.AbsRect(), optionTextAlignment, { 0,0 }, "<no options>"_fstring, colour);
+				return;
 			}
+
+			ForEachRenderedOption([this, &g](int optionIndex, const GuiRect& edge, const Option& option)
+				{
+					UNUSED(optionIndex);
+
+					GRAlignmentFlags optionTextAlignment;
+					optionTextAlignment.Add(EGRAlignment::HCentre).Add(EGRAlignment::VCentre);
+
+					bool isHovered = IsPointInRect(g.CursorHoverPoint(), edge);
+					GRRenderState rs(false, isHovered, false);
+					RGBAb colour = panel.GetColour(EGRSchemeColourSurface::GAME_OPTION_TEXT, rs);
+					g.DrawText(GRFontId::MENU_FONT, edge, edge, optionTextAlignment, { 0,0 }, to_fstring(option.value), colour);
+
+					RGBAb topLeftColour = panel.GetColour(EGRSchemeColourSurface::GAME_OPTION_TOP_LEFT, rs);
+					RGBAb bottomRightColour = panel.GetColour(EGRSchemeColourSurface::GAME_OPTION_BOTTOM_RIGHT, rs);
+					g.DrawRectEdge(edge, topLeftColour, bottomRightColour);
+				}
+			);
 		}
 
 		EGRQueryInterfaceResult QueryInterface(IGRBase** ppOutputArg, cstr interfaceId) override
