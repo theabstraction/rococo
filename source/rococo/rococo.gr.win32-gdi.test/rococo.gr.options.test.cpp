fe3685fa9d7f22a0ad33530853428cf59605280c
Shyreman <mark.anthony.taylor@gmail.com>
2025-04-17 10:12:03 +0100

    Added more demo graphic options

--- source/rococo/rococo.gr.win32-gdi.test/rococo.gr.options.test.cpp
+++ source/rococo/rococo.gr.win32-gdi.test/rococo.gr.options.test.cpp
@@ -41,6 +41,87 @@ void TestOptions(IGRClientWindow& client, IGRSystem& gr, IGameOptions& options)
 	RunMessageLoop(client);
 }
 
+struct AudioOptions : IGameOptions
+{
+	OptionDatabase<AudioOptions> db;
+
+	double musicVolume = 0.25;
+	double fxVolume = 0.2;
+	double narrationVolume = 0.4;
+	HString speakerConfig = "2";
+
+	IOptionDatabase& DB() override
+	{
+		return db;
+	}
+
+	AudioOptions() : db(*this)
+	{
+
+	}
+
+	void GetMusicVolume(IScalarInquiry& inquiry)
+	{
+		inquiry.SetTitle("Music Volume");
+		inquiry.SetRange(0, 100.0);
+		inquiry.SetActiveValue(musicVolume);
+	}
+
+	void SetMusicVolume(double value)
+	{
+		musicVolume = value;
+	}
+
+	void GetFXVolume(IScalarInquiry& inquiry)
+	{
+		inquiry.SetTitle("FX Volume");
+		inquiry.SetRange(0, 100.0);
+		inquiry.SetActiveValue(fxVolume);
+	}
+
+	void SetFXVolume(double value)
+	{
+		fxVolume = value;
+	}
+
+	void GetNarrationVolume(IScalarInquiry& inquiry)
+	{
+		inquiry.SetTitle("Narration Volume");
+		inquiry.SetRange(0, 100.0);
+		inquiry.SetActiveValue(narrationVolume);
+	}
+
+	void SetNarrationVolume(double value)
+	{
+		narrationVolume = value;
+	}
+
+	void GetSpeakerConfiguration(IChoiceInquiry& inquiry)
+	{
+		inquiry.SetTitle("Speaker Configuration");
+		inquiry.AddChoice("2", "2 (Stereo speakers)");
+		inquiry.AddChoice("2_1", "2.1 (Stereo + Subwoofer)");
+		inquiry.AddChoice("5_1", "5.1 Dolby Surround");
+		inquiry.AddChoice("7_1", "7.1 Dolby Surround");
+		inquiry.AddChoice("2H", "Stereo Headphones");
+		inquiry.SetActiveChoice(speakerConfig);
+	}
+
+	void SetSpeakerConfiguration(cstr choice)
+	{
+		speakerConfig = choice;
+	}
+
+	void AddOptions(IGameOptionsBuilder& builder) override
+	{
+		ADD_GAME_OPTIONS(db, AudioOptions, MusicVolume)
+		ADD_GAME_OPTIONS(db, AudioOptions, FXVolume)
+		ADD_GAME_OPTIONS(db, AudioOptions, NarrationVolume)
+		ADD_GAME_OPTIONS(db, AudioOptions, SpeakerConfiguration)
+		db.Build(builder);
+	}
+};
+
 struct GraphicsOptions: IGameOptions
 {
 	OptionDatabase<GraphicsOptions> db;
@@ -52,10 +133,13 @@ struct GraphicsOptions: IGameOptions
 
 	HString activeScreenMode = "Fullscreen";
 	HString shadowQuality = "2";
+	HString landscapeQuality = "1";
+	HString reflectionAlgorithm = "1";
+	HString monitor = "1";
+	HString resolution = "1920x1080";
+	HString textureQuality = "1";
+	HString waterQuality = "1";
 	bool isFSAAEnabled = false;
-	double musicVolume = 0.25;
-	double fxVolume = 0.2;
-	double narrationVolume = 0.4;
 
 	GraphicsOptions() : db(*this)
 	{
@@ -103,50 +187,132 @@ struct GraphicsOptions: IGameOptions
 		shadowQuality = value;
 	}
 
-	void GetMusicVolume(IScalarInquiry& inquiry)
+	void GetLandscapeQuality(IChoiceInquiry& inquiry)
 	{
-		inquiry.SetTitle("Music Volume");
-		inquiry.SetRange(0, 100.0);
-		inquiry.SetActiveValue(musicVolume);
+		inquiry.SetTitle("Landscape Quality");
+		inquiry.AddChoice("1", "Low");
+		inquiry.AddChoice("2", "Medium");
+		inquiry.AddChoice("3", "High");
+		inquiry.AddChoice("4", "Ultra");
+		inquiry.AddChoice("5", "Ultra (Experimental)");
+		inquiry.SetActiveChoice(landscapeQuality);
 	}
 
-	void SetMusicVolume(double value)
+	void SetLandscapeQuality(cstr value)
 	{
-		musicVolume = value;
+		landscapeQuality = value;
 	}
 
-	void GetFXVolume(IScalarInquiry& inquiry)
+	void GetReflectionAlgorithm(IChoiceInquiry& inquiry)
 	{
-		inquiry.SetTitle("FX Volume");
-		inquiry.SetRange(0, 100.0);
-		inquiry.SetActiveValue(fxVolume);
+		inquiry.SetTitle("Reflection Algorithm");
+		inquiry.AddChoice("1", "Gloss (Minimal)");
+		inquiry.AddChoice("2", "Global Environmental Mapping (Low)");
+		inquiry.AddChoice("3", "Local Environmental Mapping (Medium)");
+		inquiry.AddChoice("4", "Dynamic Environmental Mapping (High)");
+		inquiry.AddChoice("5", "Raytracing (Ultra)");
+		inquiry.SetActiveChoice(reflectionAlgorithm);
 	}
 
-	void SetFXVolume(double value)
+	void SetReflectionAlgorithm(cstr value)
 	{
-		fxVolume = value;
+		reflectionAlgorithm = value;
 	}
 
-	void GetNarrationVolume(IScalarInquiry& inquiry)
+	void GetActiveMonitor(IChoiceInquiry& inquiry)
 	{
-		inquiry.SetTitle("Narration Volume");
-		inquiry.SetRange(0, 100.0);
-		inquiry.SetActiveValue(narrationVolume);
+		inquiry.SetTitle("Active Monitor");
+		inquiry.AddChoice("1", "1 - 1920x1080");
+		inquiry.AddChoice("2", "2 - 3840x2160");
+		inquiry.SetActiveChoice(monitor);
 	}
 
-	void SetNarrationVolume(double value)
+	void SetActiveMonitor(cstr value)
 	{
-		narrationVolume = value;
+		monitor = value;
+	}
+
+	void GetFullscreenResolution(IChoiceInquiry& inquiry)
+	{
+		inquiry.SetTitle("Fullscreen Resolution");
+		inquiry.AddChoice("1920x1080", "1920 x 1080 60Hz");
+		inquiry.AddChoice("1920x1200", "1920 x 1200 60Hz");
+		inquiry.AddChoice("2560x1536", "2560 x 1536 60Hz");
+		inquiry.AddChoice("3840x2160", "3840 x 2160 60Hz");
+		inquiry.AddChoice("1366x768", "1366 x 768 60Hz");
+		inquiry.AddChoice("1280x1024", "1280 x 1024 60Hz");
+		inquiry.AddChoice("1024x768", "1024 x 768 60Hz");
+		inquiry.AddChoice("800x600", "800 x 600 60Hz");
+		inquiry.AddChoice("640x480", "640 x 480 60Hz");
+		inquiry.AddChoice("1920x1080", "1920 x 1080 144Hz");
+		inquiry.AddChoice("1920x1200", "1920 x 1200 144Hz");
+		inquiry.AddChoice("2560x1536", "2560 x 1536 144Hz");
+		inquiry.AddChoice("3840x2160", "3840 x 2160 144Hz");
+		inquiry.AddChoice("1366x768", "1366 x 768 144Hz");
+		inquiry.AddChoice("1280x1024", "1280 x 1024 144Hz");
+		inquiry.AddChoice("1024x768", "1024 x 768 144Hz");
+		inquiry.AddChoice("800x600", "800 x 600 144Hz");
+		inquiry.AddChoice("640x480", "640 x 480 144Hz");
+		inquiry.AddChoice("1920x1080", "1920 x 1080 200Hz");
+		inquiry.AddChoice("1920x1200", "1920 x 1200 200Hz");
+		inquiry.AddChoice("2560x1536", "2560 x 1536 200Hz");
+		inquiry.AddChoice("3840x2160", "3840 x 2160 200Hz");
+		inquiry.AddChoice("1366x768", "1366 x 768 200Hz");
+		inquiry.AddChoice("1280x1024", "1280 x 1024 200Hz");
+		inquiry.AddChoice("1024x768", "1024 x 768 200Hz");
+		inquiry.AddChoice("800x600", "800 x 600 200Hz");
+		inquiry.AddChoice("640x480", "640 x 480 200Hz");
+		inquiry.SetActiveChoice(resolution);
+	}
+
+	void SetFullscreenResolution(cstr choice)
+	{
+		resolution = choice;
+	}
+
+	void GetTextureQuality(IChoiceInquiry& inquiry)
+	{
+		inquiry.SetTitle("Texture Quality");
+		inquiry.AddChoice("1", "Low 256x256");
+		inquiry.AddChoice("2", "Medium 512x512");
+		inquiry.AddChoice("3", "High 1024x1024 ");
+		inquiry.AddChoice("4", "Very High 2048x2048");
+		inquiry.AddChoice("5", "Ultra 4096x2096");
+		inquiry.SetActiveChoice(textureQuality);
+	}
+
+	void SetTextureQuality(cstr choice)
+	{
+		textureQuality = choice;
+	}
+
+	void GetWaterQuality(IChoiceInquiry& inquiry)
+	{
+		inquiry.SetTitle("Water Quality");
+		inquiry.AddChoice("1", "Low - flat");
+		inquiry.AddChoice("2", "Medium - ripples");
+		inquiry.AddChoice("3", "High - ripples & refraction");
+		inquiry.AddChoice("4", "Very High - full physics");
+		inquiry.AddChoice("5", "Ultra - physics and reflections");
+		inquiry.SetActiveChoice(waterQuality);
+	}
+
+	void SetWaterQuality(cstr choice)
+	{
+		waterQuality = choice;
 	}
 
 	void AddOptions(IGameOptionsBuilder& builder) override
 	{
 		ADD_GAME_OPTIONS(db, GraphicsOptions, ScreenMode)
 		ADD_GAME_OPTIONS(db, GraphicsOptions, FSAA)
-		ADD_GAME_OPTIONS(db, GraphicsOptions, MusicVolume)
-		ADD_GAME_OPTIONS(db, GraphicsOptions, FXVolume)
-		ADD_GAME_OPTIONS(db, GraphicsOptions, NarrationVolume)
 		ADD_GAME_OPTIONS(db, GraphicsOptions, ShadowQuality)
+		ADD_GAME_OPTIONS(db, GraphicsOptions, LandscapeQuality)
+		ADD_GAME_OPTIONS(db, GraphicsOptions, ReflectionAlgorithm)
+		ADD_GAME_OPTIONS(db, GraphicsOptions, ActiveMonitor)
+		ADD_GAME_OPTIONS(db, GraphicsOptions, FullscreenResolution)
+		ADD_GAME_OPTIONS(db, GraphicsOptions, TextureQuality)
+		ADD_GAME_OPTIONS(db, GraphicsOptions, WaterQuality)
 		db.Build(builder);
 	}
 };
