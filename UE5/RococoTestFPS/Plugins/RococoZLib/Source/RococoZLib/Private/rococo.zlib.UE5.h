8b56841450c785daec5d66f978764708e278935f
theabstraction <mark.anthony.taylor@gmail.com>
2025-06-14 13:06:50 +0100

    ZLib compiles

---
+++ UE5/RococoTestFPS/Plugins/RococoZLib/Source/RococoZLib/Private/rococo.zlib.UE5.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef ROCOCO_UE5_EXPORT
+# ifdef ROCOCO_BUILD_IS_MONOLITHIC
+#  define ROCOCO_UE5_EXPORT
+#  define ROCOCO_UE5_IMPORT
+# else
+#  define ROCOCO_UE5_EXPORT DLLEXPORT
+#  define ROCOCO_UE5_IMPORT DLLIMPORT
+# endif
+#endif
+
+#define __ROCOCO_WIDECHAR__ WIDECHAR
+
+#ifdef PLATFORM_ANDROID
+# define ROCOCO_WIDECHAR_IS_CHAR_16_T
+#endif
+
+#ifdef _WIN32
+# define ROCOCO_WIDECHAR_IS_WCHAR_T
+#endif
