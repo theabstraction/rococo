1a5fa719f64fec1af48f50c685ba27e80b32bd94
theabstraction <mark.anthony.taylor@gmail.com>
2025-06-14 17:59:39 +0100

    Stupid deprecation warnings from Microsoft causing problems. Hopefully they are fixed.

--- UE5/RococoTestFPS/Plugins/RococoTiffLib/Source/RococoTiffLib/Private/rococo.tiff.prelude.h
+++ UE5/RococoTestFPS/Plugins/RococoTiffLib/Source/RococoTiffLib/Private/rococo.tiff.prelude.h
@@ -1,6 +1,6 @@
 #ifdef _WIN32
-# define _CRT_SECURE_NO_WARNINGS 
 # pragma warning(disable: 4456)
 # pragma warning(disable: 4459)
+# pragma warning(disable: 4996)
 #else
 #endif
--- source/3rd-Party/libtiff/bloke.tiff.cpp
+++ source/3rd-Party/libtiff/bloke.tiff.cpp
@@ -1,3 +1,7 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+# define _CRT_SECURE_NO_WARNINGS 
+#endif
+
 #ifndef _WIN32
 # include <stddef.h>
 #else
