507607c648a8b16eaccd329bdbac541733cf8350
theabstraction <mark.anthony.taylor@gmail.com>
2025-06-27 11:01:41 +0100

    Improved package-android.bat

--- UE5/RococoTestFPS/package-android.bat
+++ UE5/RococoTestFPS/package-android.bat
@@ -22,14 +22,12 @@ set logfile=%~dp0package-android.log
 echo BuildCookRun underway. Please be patient
 
 %UEBF%RunUAT.bat ^
-BuildCookRun -project=%~dp0RococoTestFPS.uproject ^
--ScriptsForProject=%~dp0RococoTestFPS.uproject ^
-Turnkey -command=VerifySdk -platform=Android -UpdateIfNeeded ^
-BuildCookRun -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook ^
--project=%~dp0RococoTestFPS.uproject -target=RococoTestFPS ^
--unrealexe=%UE%Engine\Binaries\Win64\UnrealEditor-Cmd.exe ^
--platform=Android -cookflavor=ASTC -stage -archive -package -build -pak -iostore -compressed -prereqs ^
--archivedirectory=%~dp0Packaged -clientconfig=Shipping -nocompile -nocompileuat ^
--stagingdirectory=%~dp0Packaged
-
+-ScriptsForProject=%~dp0RococoTestFPS.uproject^
+Turnkey -command=VerifySdk -platform=Android -UpdateIfNeeded -EditorIO -EditorIOPort=51082^
+-project=%~dp0RococoTestFPS.uproject^
+BuildCookRun -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook^
+-project=%~dp0RococoTestFPS.uproject -target=RococoTestFPS^ 
+-unrealexe="D:\work\rococo\UE5\RococoTestFPS\Binaries\Win64\RococoTestFPSEditor-Cmd.exe" -plat^
+form=Android  -cookflavor=ASTC -stage -archive -package -build -pak -iostore -compressed -prereqs^
+-archivedirectory=%~dp0RococoTestFPS/Packaged -clientconfig=Development" -nocompile -nocompileuat^
 REM > %logfile% 2>&1
