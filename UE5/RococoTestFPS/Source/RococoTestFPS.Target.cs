a605e3a21af0d72e1642e5aabda03e19231dfcf0
Mark Anthony Taylor <theabstraction@hotmail.com>
2025-10-10 10:36:09 +0100

    Added some comments to target file to indicate how to fix stand-alone errors if the wrong branch has been applied for stand-alone.

--- UE5/RococoTestFPS/Source/RococoTestFPS.Target.cs
+++ UE5/RococoTestFPS/Source/RococoTestFPS.Target.cs
@@ -10,6 +10,10 @@ public class RococoTestFPSTarget : TargetRules
 {
     public RococoTestFPSTarget(TargetInfo Target) : base(Target)
 	{
+		// Comment out TargetBuildEnvironment.Unique and bUseLoggingInShipping if you want to run with the stand-alone, non-locally-compiled version of UE.
+		// If you have synched with some branch other than a UE5-GR-RC, you may find it was not commented out by default, as working copies in the master branch
+		// are used with a locally-compiled version of UE. Switch branch to fix or add comment characters.
+		// TODO - investigate possibility of detecting type of UE5 installation automatically and apply procedurally.
        // BuildEnvironment = TargetBuildEnvironment.Unique;
         //bUseLoggingInShipping = true;
 		Type = TargetType.Game;
