93dc661b2f50113c1f51ff92eb3d9da56dbb264e
theabstraction <mark.anthony.taylor@gmail.com>
2025-10-11 13:28:43 +0100

    Added explicit copyright banners to the gui retained cpp modules - to keep FAB happy

--- source/rococo/rococo.gui.retained/dllmain.cpp
+++ source/rococo/rococo.gui.retained/dllmain.cpp
@@ -1,3 +1,4 @@
+// Copyright (c)2025 Mark Anthony Taylor. Email: mark.anthony.taylor@gmail.com. All rights reserved.
 #include <rococo.types.h>
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
