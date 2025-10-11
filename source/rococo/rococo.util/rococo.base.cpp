17aa014ad2081c154b623d756ef22f5cc281d0ce
theabstraction <mark.anthony.taylor@gmail.com>
2025-10-11 13:21:16 +0100

    Added explicit copyright banenrs to the rococo.util folder

--- source/rococo/rococo.util/rococo.base.cpp
+++ source/rococo/rococo.util/rococo.base.cpp
@@ -1,3 +1,4 @@
+// Copyright (c)2025 Mark Anthony Taylor. Email: mark.anthony.taylor@gmail.com. All rights reserved.
 #ifndef ROCOCO_API
 # error "Define ROCOCO_API"
 #endif
--- source/rococo/rococo.util/rococo.strings.cpp
+++ source/rococo/rococo.util/rococo.strings.cpp
@@ -1,3 +1,4 @@
+// Copyright (c)2025 Mark Anthony Taylor. Email: mark.anthony.taylor@gmail.com. All rights reserved.
 #include <rococo.types.h>
 
 #define _CRT_NONSTDC_NO_WARNINGS
